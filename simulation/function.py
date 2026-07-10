1e02e5509d09adb77cdbfc2eaffe7b1eb5cd6628
radiowavepredictor <radiowavepredictor@gmail.com>
2026-07-10 15:52:23 +0900

    マルチパスとシャドウイング統合

--- simulation/function.py
+++ simulation/function.py
@@ -58,14 +58,28 @@ def make_shadowing(simu_cfg, rnd):
     d_c=simu_cfg.d_c 
     
     a=0.5**(simu_cfg.delta_d/d_c)
-    sigma_w=sigma_shadow*np.sqrt(1-a**2)
     shadow=np.zeros(data_num)
-    
+    b=np.zeros(data_num)
+
+    b[0]=rnd.normal(0,sigma_shadow)
+    shadow[0]=b[0]
+
     for n in range(1,data_num):
-        w=rnd.normal(0,sigma_w)
-        shadow[n]=a*shadow[n-1]+w
+        b[n]=rnd.normal(0,sigma_shadow)
+
+        shadow[n]=a*shadow[n-1]+np.sqrt(1-a**2)*b[n]
         
     return shadow
+ #マルチパスとシャドウイングの統合
+def make_rice_shadowing(simu_cfg, rnd):
+    fading = make_rice_fading(simu_cfg, rnd)
+    power = np.abs(fading)**2
+    power_db = mw_to_dbm(power)
+    shadow = make_shadowing(simu_cfg, rnd)
+    receive_power = power_db + shadow
+    return receive_power
+    
+
 
 
 ### シミュレーション用のデータセット(入力と答え)をdata_set_num分用意する関数
@@ -82,6 +96,11 @@ def make_rice_dataset(
     rice_wave_arr = np.array(rice_wave_arr)
     power_rice_wave_arr = np.abs(rice_wave_arr) ** 2
     power_db_rice_wave_arr = mw_to_dbm(power_rice_wave_arr)
+
+    plt.figure(figsize=(10,4))
+    plt.plot(rice_wave_arr[0])
+    plt.grid(True)
+    plt.show()
 
     # 標準化
     if scaler is None:
@@ -150,6 +169,49 @@ def make_shadow_learning_dataset(
     val_dataset, scaler = make_shadow_dataset(rnn_cfg, val_simu_cfg, rnd, scaler)
     return train_dataset, val_dataset, scaler
     
+###シミュレーション用のデータセット(マルチパス＋シャドウイング版)
+def make_rice_shadow_dataset(
+    rnn_cfg: RnnConfig,
+    simu_cfg: SimulationConfig,
+    rnd: RandomState,
+    scaler: StandardScaler | None = None,
+):
+    # シャドウイングの応答波の配列 [h1,h2,h3…] を作る
+    rice_shadow_wave_arr = []
+    for _ in range(simu_cfg.data_set_num):
+        rice_shadow_wave_arr.append(make_rice_shadowing(simu_cfg,rnd))
+    rice_shadow_wave_arr = np.array(rice_shadow_wave_arr)
+
+    plt.figure(figsize=(10,4))
+    plt.plot(rice_shadow_wave_arr[0])
+    plt.grid(True)
+    plt.show()
+
+   
+    # 標準化
+    if scaler is None:
+        scaler = StandardScaler()
+        scaler.fit(rice_shadow_wave_arr.reshape(-1, 1))
+    data_norm_arr = scaler.transform(rice_shadow_wave_arr.reshape(-1, 1)).reshape(
+        rice_shadow_wave_arr.shape
+    )
+
+    dataset = array_of_array_to_dataset(data_norm_arr, rnn_cfg)
+    return dataset, scaler
+
+
+def make_rice_shadow_learning_dataset(
+        simu_cfg,rnn_cfg,rnd
+):
+    train_dataset, scaler = make_rice_shadow_dataset(rnn_cfg, simu_cfg, rnd)
+    # 検証用のデータセットをつくるために、データセットの数を訓練用の1/4に゙設定し直す
+    val_simu_cfg = simu_cfg.model_copy(
+        update={"data_set_num": simu_cfg.data_set_num // 4}
+    )
+    val_dataset, scaler = make_rice_shadow_dataset(rnn_cfg, val_simu_cfg, rnd, scaler)
+    return train_dataset, val_dataset, scaler
+
+
 
 def predict_multiple_waves(
     model,
@@ -164,17 +226,27 @@ def predict_multiple_waves(
     1回目のデータセットだけ詳細な情報を返す
     """
     # 中上ライスのデータを取得(kerasモデルに渡せるように加工されていない状態)
-    shadow_db=make_shadowing(simu_cfg,rnd)
+    #shadow_db=make_shadowing(simu_cfg,rnd)
     predict_num = simu_cfg.predicted_dataset_num
     rmse_sum = Counter({})
     for i in range(predict_num):
+
+        #マルチパスフェージングのみ
         #fading_data = make_rice_fading(simu_cfg,rnd)
         #power = np.abs(fading_data) ** 2
         #power_db = 10 * np.log10(power)
-        #power_db = shadow_db.reshape(-1, 1)
+        #power_db = power_db.reshape(-1, 1)
+        #rice_data=make_rice_fading(simu_cfg,rnd)
+        #input_data=rice_data.reshape(-1,1)
 
+        #シャドウイングのみ
         shadow_data=make_shadowing(simu_cfg,rnd)
         input_data=shadow_data.reshape(-1,1)
+
+
+        #マルチパスとシャドウイング統合
+        #rice_shadow_data=make_rice_shadowing(simu_cfg,rnd)
+        #input_data=rice_shadow_data.reshape(-1,1)
 
         plt.close("all")
         result_i = predict(
